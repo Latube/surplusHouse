1abbf9565126599bb7081603564faa8fdc4c4333
tangtao <ytopt@hotmail.com>
2018-08-19 11:53:00 +0800

    modified

--- presellNum.js
+++ presellNum.js
@@ -6,25 +6,3 @@ let cdfx = "https://www.cdfangxie.com";
 let topicUrls = [];
 // var app = express();
 
-for (let i = 1; i <= 26; i++) {
-  var pageUrl = "https://www.cdfangxie.com/Infor/type/typeid/36.html?&p=" + i;
-  superagent.get(pageUrl).end(function (err, sres) {
-    if (err) {
-      console.err(err);
-    }
-
-    /**
-     * 爬取预售楼盘详情的链接和区域
-     */
-    let $ = cheerio.load(sres.text);
-    $('.ul_list a').each(function (idx, element) {
-      let $element = $(element);      
-      topicUrls.push({
-        href: url.resolve(cdfx, $element.attr('href')),
-        zone: $element.attr('title')
-      });
-    });
-
-    console.log(topicUrls);
-  });
-}
